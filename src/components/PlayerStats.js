af983e3c7e7322ba0177dada4aaf47937827b679
unknown <chris.a.desilva@gmail.com>
2019-03-28 10:54:29 -0500

    Add dropdown of teams, show score of team selected onChange

--- src/components/PlayerStats.js
+++ src/components/PlayerStats.js
@@ -9,36 +9,79 @@ class PlayerStats extends Component {
       awayTeam: undefined,
       awayScore: undefined,
       gameStatus: undefined,
-      period: undefined,
+      teams: ["Teams"],
+      teamID: null,
       error: undefined
     }
   }
 
-  componentDidMount(){
-    fetch("https://www.balldontlie.io/api/v1/games")
+
+  componentDidMount() {
+    fetch("https://www.balldontlie.io/api/v1/teams")
+    .then(res => res.json())
+    .then( result => {
+      console.log(result)
+      this.setState({
+        teams: result.data
+      })
+    })
+    .catch(error => this.setState({ error: "Please try again" }))
+  }
+
+  // getStats = async (e) => {
+  //   e.preventDefault();
+  //   await fetch("https://www.balldontlie.io/api/v1/teams")
+  //   .then(res => res.json())
+  //   .then( result => {
+  //     console.log(result)
+  //     this.setState({
+  //       homeTeam: result.data[0].home_team.full_name,
+  //       homeScore: result.data[0].home_team_score,
+  //       awayTeam: result.data[0].visitor_team.full_name,
+  //       awayScore: result.data[0].visitor_team_score,
+  //       gameStatus: result.data[0].status
+  //     })
+  //   })
+  //   .catch(error => this.setState({ error: "Please try again" }))
+  // }
+
+    getScore = (e) => {
+      this.setState({ teamID: e.target.value })
+    }
+
+    getGame = async (e) => {
+      e.preventDefault();
+      await fetch(`https://www.balldontlie.io/api/v1/games/?seasons[]=2018&&team_ids[]=${this.state.teamID}`)
       .then(res => res.json())
       .then( result => {
         console.log(result)
         this.setState({
-          homeTeam: result.data[0].home_team.full_name,
-          homeScore: result.data[0].home_team_score,
-          awayTeam: result.data[0].visitor_team.full_name,
-          awayScore: result.data[0].visitor_team_score,
-          gameStatus: result.data[0].status,
-          period: result.data[0].period
+          homeTeam: result.data[result.data.length - 1].home_team.full_name,
+          homeScore: result.data[result.data.length - 1].home_team_score,
+          awayTeam: result.data[result.data.length - 1].visitor_team.full_name,
+          awayScore: result.data[result.data.length - 1].visitor_team_score,
+          gameStatus: result.data[result.data.length - 1].status
         })
       })
-      .catch(error => console.log(error))
-  } 
+      .catch(error => this.setState({ error: "Please select a team from the dropdown menu" }))
+    }
 
   render() {
     return (
       <div>
         <div>
-          {this.state.homeTeam} : {this.state.homeScore} - {this.state.awayTeam} : {this.state.awayScore}
+          {this.state.homeTeam && <p>{this.state.homeTeam} : {this.state.homeScore} - {this.state.awayTeam} : {this.state.awayScore}</p>}
         </div>
         <div>
-          {this.state.period} / {this.state.gameStatus}
+          {this.state.gameStatus}
+        </div>
+        <div>
+          <button onClick={this.getGame}>Get Stats</button>
+          {this.state.error && <p>{this.state.error}</p>}
+          <select onChange={this.getScore}>
+            <option>Teams</option>
+            {this.state.teams.map( team => { return <option key={team.id} value={team.id}>{team.abbreviation}</option> } )}
+          </select>
         </div>
       </div>
     )
